c8787724cd34f64e7f2f9fb8b55bf0df4de993d1
手瓜一十雪 <nineto0@163.com>
2023-05-27 19:09:14 +0800

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,7 +1,7 @@
 const fs = require('fs').promises;
 const path = require('path');
 var lang = null;
-var langcode = null;
+var langCode = null;
 async function loadLanguage(languageCode) {
   try {
     languageCode = langcode;
