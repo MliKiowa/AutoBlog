2524eded6af786e3dc6130e3ef28b14bbcb21c3b
手瓜一十雪 <nineto0@163.com>
2023-05-27 18:40:47 +0800

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,28 +1,22 @@
 const fs = require('fs').promises;
 const path = require('path');
-const minimist = require('minimist');
-let lang = null;
+var lang = null;
+var langcode = null;
 async function loadLanguage(languageCode) {
   try {
+    languageCode = langcode;
     const data = await fs.readFile(
       path.join(__dirname, 'Languages', `${languageCode}.json`),
       'utf-8'
     );
-    return JSON.parse(data);
+    lang = JSON.parse(data);
   } catch (error) {
     console.error(`Error loading langauge file for ${languageCode}: ${error}`);
     return undefined;
   }
 }
-const defaultLang= 'zh-cn';
-if (process.argv.length <= 2) {
-  console.log('No command line arguments provided.');
-}
-const args = minimist(process.argv.slice(2));
-const langCode = args.lang || defaultLang;
-const lang = await loadLanguage(langCode);
 module.exports = {
+  loadLanguage:loadLanguage,
   langCode: langCode,
   lang:lang
 };
-
