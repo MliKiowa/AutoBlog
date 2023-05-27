e9349d14c6b0c8ebf9303f219d27ccee215db995
手瓜一十雪 <nineto0@163.com>
2023-05-27 19:35:57 +0800

    Update lang.js

--- lang.js
+++ lang.js
@@ -5,7 +5,7 @@ var langCode = null;
 function loadLanguage(languageCode) {
   try {
     langCode = languageCode;
-    const data = fs.readFile(
+    const data = fs.readFileSync(
       path.join(__dirname, 'Languages', `${languageCode}.json`),
       'utf-8'
     );
