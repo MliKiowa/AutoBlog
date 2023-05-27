75ab6f8006d7d2e09722465344f30daac0703ce9
手瓜一十雪 <nineto0@163.com>
2023-05-27 18:13:39 +0800

    Update boot.js

--- boot.js
+++ boot.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-function loadLanguageFile(languageCode) {
+function loadLanguage(languageCode) {
   try {
     const data = fs.readFileSync(`./Languages/${languageCode}.json`);
     const language = JSON.parse(data);
@@ -11,10 +11,15 @@ function loadLanguageFile(languageCode) {
 }
 const defaultLang= 'zh-cn'; // 语言代码，例如 'en' 表示英语
 const minimist = require('minimist');
+if (process.argv.length <= 2) {
+  // 处理没有传递任何参数的情况
+  console.log('No command line arguments provided.');
+} 
 // 解析命令行参数
 const args = minimist(process.argv.slice(2));
-// 获取语言参数，如果没有则默认为 'en'
-const langCode = args.lang || defaultLang;
-global.lang = loadLanguageFile(langCode);
-
-
+// 获取语言参数，如果没有则默认为 'zh-cn'
+global.langCode = args.lang || defaultLang;
+global.lang = loadLanguage(langCode);
+// 挂载语言到 Global
+const action = args.action || "all";
+//运行action来自 Action库 在运行前加载所有脚手架
