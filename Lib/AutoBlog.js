1dd460fdc930e223b763a4e74ff6f9f6943b55ca
手瓜一十雪 <nineto0@163.com>
2023-05-27 20:15:00 +0800

    Create AutoBlog.js

---
+++ Lib/AutoBlog.js
@@ -0,0 +1,24 @@
+var langHelper = require("./lang.js");
+var lang = langHelper.lang();
+function runShellScript(scriptName, callback) {
+  const { exec } = require('child_process');
+  const path = require('path'); 
+  const scriptPath = path.join(__dirname, '../Script', scriptName);
+  exec(`sh ${scriptPath}`, (error, stdout, stderr) => {
+  if (error) {
+      callback(lang["error"]+`：${error}`);
+      return;
+    }
+    callback(null, {
+      stdout: stdout,
+      stderr: stderr
+    });
+  });
+}
+function method2() {
+  console.log('这是方法2');
+}
+module.exports = {
+  langHelper,
+  method2
+};
