496fa4db00dc2a09d385e576f767c825016d4154
手瓜一十雪 <nineto0@163.com>
2023-05-27 18:46:00 +0800

    Update boot.js

--- boot.js
+++ boot.js
@@ -5,5 +5,6 @@ const langCode = args.lang || defaultLang;
 // 语言加载
 langHelper.loadLanguage(langCode);
 var lang = langHelper.lang;
+var langCode= langHelper.langCode;
 const action = args.action || "all";
 //运行action来自 Action库 在运行前加载所有脚手架
